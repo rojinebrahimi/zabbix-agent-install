59ded93da1fb4bd4dd9706aee58d5bce79ad77d5
rojinebrahimi <rojin.ebrahimi2014@gmail.com>
2020-08-12 16:29:30 +0430

    tabs replaced by spaces

--- installzabbix.py
+++ installzabbix.py
@@ -1,47 +1,53 @@
+#!/bin/sh
 #zabbix-agent installer
 import sys
 import os
-import commands
 import socket
 import re
-
-# Getting Host IP
-#def get_host_IP():
-#    try:
-#	 host_ip = socket.gethostbyname(socket.gethostname())
-#	 print('-----------------Reaching host IP-------------------')
-#	 return host_ip
-#    except:
-#      	 print('--------------host IP is not reachable--------------')
+import subprocess
 
 # Getting Host Name
 def get_host_name():
-    try:
-	host_name = socket.gethostname()
-	return host_name
-    except:
-	print('-----------------Hostname not reachable-----------------')
+    hostname=""
+    print("1. Machine's Hostname\n2. Enter Hostname Manually\n")
+    select_host_name = input("Select the hostname (1, 2): ")
+    if select_host_name == "1":
+        hostname = socket.gethostname()
+        
+    elif select_host_name == "2":
+	    hostname = input("Enter the hostname, please: ")
+    	   
+    return hostname
+
+# Check Hostname Validation
+def valid_host_name(hostname):
+    if hostname != "" or hostname != None or hostname != "localhost":
+	    return True
+    else:
+	    return False			    
+    return hostname
+
 
 # Remove Existing Zabbix Agent
 def remove_zabbix_agent():
     try:
-	os.system('sudo yum remove zabbix-release')
-	os.system('sudo yum remove zabbix-agent')
-  	run = commands.getoutput('sudo ps -ef | grep zabbix_agentd | grep -v grep')
-	file = os.path.exists('/etc/zabbix')
-	if run:
-		os.system('sudo service zabbix-agent stop')
-		print("Zabbix agent service stopped")
-	if file:
-		os.system('sudo rm -rf /etc/zabbix')
-		print("Zabbix removed")
+        os.system('sudo yum remove zabbix-release')
+        os.system('sudo yum remove zabbix-agent')
+        run = subprocess.getstatusoutput('sudo ps -ef | grep zabbix_agentd | grep -v grep')
+        file_ = os.path.exists('/etc/zabbix')
+        if run:
+            os.system('sudo service zabbix-agent stop')
+            print("Zabbix agent service stopped")
+        if file_:
+            os.system('sudo rm -rf /etc/zabbix')
+            print("Zabbix removed")
     except Exception as e:
-	print(e)	
+        print(e)	
 
 
 # Asking Zabbix Server IP Address
 def zabbix_server_IP():
-    server_IP = input("Enter the zabbix server IP, please (exm: '127.0.0.1'): ") 
+    server_IP = input("Enter the zabbix server IP, please (exm: 192.168.X.X ): ") 
     return server_IP
 
 
@@ -49,77 +55,83 @@ def zabbix_server_IP():
 def valid_IP(server_IP):
     IP_add = re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', server_IP)
     if IP_add:
-	return True
+        return True
     else:
-	return False
+        return False
 	
 # Install Zabbix Agent
 def install_zabbix_agent():
     try:
-    	installed = False
-	remove_zabbix_agent()
-	print("-----------------Disabling SELINUX------------------")
-	os.system("sudo sed -i 's/^SELINUX=/SELINUX=permissive' /etc/selinux/config")
+        installed = False
+        remove_zabbix_agent()
+        print("-----------------Disabling SELINUX------------------")
+        os.system("sudo setenforce 0")
 	
-	
-	print("-----------------Installation Started, Updating----------------\n")
-	os.system("sudo yum update")
-	
-	print("----------------Installing repo----------------\n")
-	version = input("Enter the version of zabbix to install(3, 4, 5): ")
-	centos_version = input("Enter the version of CentOs(6, 7): ")
-	try:
-		repo = 'https://repo.zabbix.com/zabbix/%s.0/rhel/%s/x86_64/zabbix-release-%s.0-1.el%s.noarch.rpm'%(version, centos_version, version, centos_version)
-		os.system('sudo yum install %s'%(repo))
-	except Exception as e:
-		print(e)
-		system.exit()
-
-	print("--------------------Getting zabbix repo-------------------\n")
-	get_repo = commands.getoutput('sudo yum list installed | grep zabbix-release')
-	
-	if get_repo:
-		print("---------------------Repo downloaded---------------------\n")
-		os.system('sudo yum install zabbix-agent')
-		os.system('systemctl start zabbix-agent')
+	    #print("-----------------Installation Started, Updating----------------\n")
+	    #os.system("sudo yum update")
 		
-		print("---------------------Server/Server Active Configuration--------------------\n")
-		while True:
-		    server_IP = zabbix_server_IP()
-		    if valid_IP(server_IP):
-			print("---------------------Server IP Confirmed---------------------\n")
-			os.system("sudo sed -i 's/^Server=.*/Server="+(server_IP)+"/g' /etc/zabbix/zabbix_agentd.conf")
-			os.system("sudo sed -i 's/^ServerActive=.*/ServerActive="+(server_IP)+"/g' /etc/zabbix/zabbix_agentd.conf")
-			os.system("echo 'HostMetadata=auto_install' >> /etc/zabbix/zabbix_agentd.conf")
-			break
-		    else:
-			print("Server IP was not confirmed, try again...\n")
-			continue
-								
+        print("----------------Installing repo----------------\n")
+        version = input("Enter the version of zabbix to install(3, 4, 5): ")
+        centos_version = input("Enter the version of CentOs(6, 7): ")
+        try:
+            repo = 'https://repo.zabbix.com/zabbix/%s.0/rhel/%s/x86_64/zabbix-release-%s.0-1.el%s.noarch.rpm'%(version, centos_version, version, centos_version)
+            os.system('sudo yum install %s'%(repo))
+        except Exception as e:
+            print(e)
+            system.exit()
 
-		print('--------------------Hostname Configuration-------------------\n')
-		os.system("sudo sed -i 's/Hostname=.*/Hostname=%s/g' /etc/zabbix/zabbix_agentd.conf"%get_host_name())
-		print("Hostname Set: %s"%get_host_name())
-	
-		print('-------------------Firewall Configuration------------------\n')
-		os.system('sudo firewall-cmd --add-port=10050/tcp --permanent')
-		os.system('sudo firewall-cmd --reload')
+        print("--------------------Getting zabbix repo-------------------\n")
+        get_repo = subprocess.getstatusoutput('sudo yum list installed | grep zabbix-release')
 		
-		print('--------------------Starting zabbix, Checking status-------------------\n')
-		os.system('systemctl restart zabbix-agent')
-		status = commands.getoutput('chkconfig zabbix-agent on')
-		run = commands.getoutput('sudo ps -ef | grep zabbix_agentd | grep -v grep')
-		if run:
-			print("--------------------Installation completed successfully!------------------\n")
-			installed = True
-			os.system('service zabbix-agent status')
-		else:
-			print(status)
-	else:
-		print("------------------Could not find zabbix-agent and install zabbix------------------")
+        if get_repo:
+            print("---------------------Repo downloaded---------------------\n")
+            os.system('sudo yum install zabbix-agent')
+            os.system('systemctl start zabbix-agent')
+			
+            print("---------------------Server/Server Active Configuration--------------------\n")
+            while True:
+                server_IP = zabbix_server_IP()
+                print(server_IP)
+                if valid_IP(server_IP):
+                    print("---------------------Server IP Confirmed---------------------\n")
+                    os.system("sudo sed -i 's/^Server=.*/Server="+server_IP+"/g' /etc/zabbix/zabbix_agentd.conf")
+                    os.system("sudo sed -i 's/^ServerActive=.*/ServerActive="+server_IP+"/g' /etc/zabbix/zabbix_agentd.conf")
+                    print("Zabbix server set at: "+server_IP)
+                    break
+                else:
+                    print("Server IP was not confirmed, try again...\n")
+                    continue
+									
+
+            print('--------------------Hostname Configuration-------------------\n')
+            while True:
+                user_host_name = get_host_name()
+                if valid_host_name(user_host_name):
+                    os.system("sudo sed -i 's/Hostname=.*/Hostname="+user_host_name+"/g' /etc/zabbix/zabbix_agentd.conf")
+                    print("Hostname Set: "+user_host_name)
+                    break
+                else:
+                    print("Hostname was not confirmed, try again...\n")
+                    continue	
+		
+            print('-------------------Firewall Configuration------------------\n')
+            os.system('sudo firewall-cmd --add-port=10050/tcp --permanent')
+            os.system('sudo firewall-cmd --reload')
+			
+            print('--------------------Starting zabbix, Checking status-------------------\n')
+            os.system('systemctl restart zabbix-agent')
+            status = subprocess.getstatusoutput('chkconfig zabbix-agent on')
+            run = subprocess.getstatusoutput('sudo ps -ef | grep zabbix_agentd | grep -v grep')
+            if run:
+                print("--------------------Installation completed successfully!------------------\n")
+                installed = True
+                os.system('service zabbix-agent status')
+            else:
+                print(status)
+        else:
+            print("------------------Could not find zabbix-agent and install zabbix------------------")
     except Exception as e:
-	print(e)
-  
+        print(e)
     finally:
         return installed
 
